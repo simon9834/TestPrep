c4bfbc927db4380b90b72c7147777cd1a3fc4861
Simon Hloska <hloska@spsejecna.cz>
2024-04-21 17:44:42 +0200

    preparations for test done 3.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Main {
+    public static void main(String[] args){
+/*
         Trainer tr = new Trainer();
 
         Pokémon po = new Pokémon("kokos", 33, "vodní");
@@ -8,6 +10,14 @@ public class Main {
         Pokémon pu = new Pokémon("koks", 33, "vodní");
         tr.addPokemon(pu);
         System.out.println(tr.returnList("vodní"));
+        */
+        Soubory sb = new Soubory();
+        try{
+            sb.writeToFile("C:/java/zapad.txt.txt","C:/java/zapadOut.txt.txt");
+        }catch (IOException e){
+            System.out.println("bruh");
+        }
+
 
     }
 }
--- src/Soubory.java
+++ src/Soubory.java
@@ -1,3 +1,24 @@
-public class Soubory {
+import java.io.*;
 
+public class Soubory {
+    private String text;
+
+    public void writeToFile(String filePath, String endFilePath) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(endFilePath));
+        char[] chars;
+        while ((text = br.readLine()) != null) {
+            chars = text.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                if (Character.isUpperCase(chars[i])) {
+                    chars[i] = Character.toLowerCase(chars[i]);
+                } else if (Character.isLowerCase(chars[i])) {
+                    chars[i] = Character.toUpperCase(chars[i]);
+                }
+                bw.write(chars[i]);
+            }
+        }
+        br.close();
+        bw.close();
+    }
 }
